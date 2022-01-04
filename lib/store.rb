04e7c720a9a0c510defa9564238ed11230a726d2
LeonXZhou <leon.zhou@queensu.ca>
2022-01-04 01:36:20 +0000

    added bonus validation for having atleast one type of clothing apparel

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,11 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :must_have_clothing
+  def must_have_clothing
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "must have atleast one type of clothing")
+      errors.add(:womens_apparel, "must have atleast one type of clothing")
+    end
+  end
 end
